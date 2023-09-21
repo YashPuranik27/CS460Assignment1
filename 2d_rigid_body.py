0ec48ed0b427a0d9a8f92ec298d7a78eb3567e1e
Yash Puranik <puranik.yash@gmail.com>
2023-09-21 13:41:03 -0400

    printing "collision_checking_polygons.npy"

---
+++ 2d_rigid_body.py
@@ -0,0 +1,7 @@
+import numpy as np
+from PIL import Image
+
+data = np.load(r'C:\Users\puran\Downloads\assignment1_student-1\assignment1_student\collision_checking_polygons.npy',
+               allow_pickle=True)
+
+print(data)
