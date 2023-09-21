79dd334e20a218239d973f38d1bfdf746b007eef
Yash Puranik <puranik.yash@gmail.com>
2023-09-21 13:57:50 -0400

    commented out the collision_checking_polygons.npy and imported some modules from the libraries. Also started on some starter functions.

--- 2d_rigid_body.py
+++ 2d_rigid_body.py
@@ -1,7 +1,26 @@
 import numpy as np
-from PIL import Image
+# from PIL import Image
 
-data = np.load(r'C:\Users\puran\Downloads\assignment1_student-1\assignment1_student\collision_checking_polygons.npy',
-               allow_pickle=True)
+# data = np.load(r'C:\Users\puran\Downloads\assignment1_student-1\assignment1_student\collision_checking_polygons.npy',
+# allow_pickle=True)
 
-print(data)
+# print(data)
+
+import random
+import math
+
+from scipy.spatial import ConvexHull
+import matplotlib.pyplot as plt
+
+
+def generate_convex_polygon(P, N_min, N_max, r_min, r_max):
+    polygons = []
+
+
+
+def main():
+
+
+
+if __name__ == "__main__":
+    main()
