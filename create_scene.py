f6317c16d888d4e985be9c45b090bd874d56f07f
Yash Puranik <puranik.yash@gmail.com>
2023-09-21 15:42:43 -0400

    added .concatnate in def plot_polygons. Also change file load and save method. Program opens a SciView window when ran and generates polygons.

--- create_scene.py
+++ create_scene.py
@@ -8,7 +8,6 @@ import numpy as np
 
 import random
 import math
-
 from scipy.spatial import ConvexHull
 import matplotlib.pyplot as plt
 
@@ -16,15 +15,43 @@ import matplotlib.pyplot as plt
 def generate_convex_polygon(P, N_min, N_max, r_min, r_max):
     polygons = []
 
+    for _ in range(P):
+        x_center, y_center = random.uniform(-100, 100), random.uniform(-100, 100)  # Random center of the polygon
+        N = random.randint(N_min, N_max)  # Number of vertices
+
+        points = []
+        for _ in range(N):
+            alpha_n = math.radians(random.uniform(0, 360))  # Angle in radians
+            r = random.uniform(0, r_max - r_min) + r_min
+            x = x_center + r * math.cos(alpha_n)
+            y = y_center + r * math.sin(alpha_n)
+            points.append([x, y])
+
+        hull = ConvexHull(points)
+        convex_polygon = [points[i] for i in hull.vertices]
+        polygons.append(convex_polygon)
+
+    return polygons
+
 
 def save_polygons_to_file(polygons, filename):
-    np.save(filename, polygons)
+    # Convert list of polygons to a list of np arrays
+    np_polygons = [np.array(polygon) for polygon in polygons]
+    np.savez(filename, *np_polygons)
 
 
 def load_polygons_from_file(filename):
-    return np.load(filename, allow_pickle=True).tolist()
+    with np.load(filename, allow_pickle=True) as data:
+        return [data[arr] for arr in data]
 
-# add a function to plot the polygons
+
+def plot_polygons(polygons):
+    plt.figure(figsize=(10, 10))
+    for polygon in polygons:
+        polygon = np.concatenate((polygon, [polygon[0]]), axis=0)  # close the polygon
+        xs, ys = zip(*polygon)
+        plt.plot(xs, ys)
+    plt.show()
 
 
 def main():
@@ -33,14 +60,13 @@ def main():
     N_max = int(input("Enter the maximum number of vertices (N_max): "))
     r_min = float(input("Enter the minimum radius of the polygon (r_min): "))
     r_max = float(input("Enter the maximum radius of the polygon (r_max): "))
-    filename = input("Enter the filename to save/load polygons (without extension): ") + '.npy'
+    filename = input("Enter the filename to save/load polygons (without extension): ") + '.npz'
 
-
+    polygons = generate_convex_polygon(P, N_min, N_max, r_min, r_max)
     save_polygons_to_file(polygons, filename)
 
     loaded_polygons = load_polygons_from_file(filename)
-
-
+    plot_polygons(loaded_polygons)
 
 
 if __name__ == "__main__":
